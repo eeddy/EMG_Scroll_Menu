378d8587039dccd372a7f5bc67ecf369cd82ba19
JeremyRobichaud <50625666+JeremyRobichaud@users.noreply.github.com>
2023-04-05 08:48:53 -0300

    Added Build-Time Logs
    Logs were only previously printing on the test-window, now displaying on build too

--- Assets/Logger.cs
+++ Assets/Logger.cs
@@ -10,20 +10,31 @@ using System.Text;
 
 public class Logger
 {
-  public string fileName;
+  public string testFileName;
+  public string buildFileName;
 
   public Logger()
   {
     var todayDate = DateTime.Now;
 		string strToday = todayDate.ToString("[MM-dd-yyyy][HH;mm;ss]");
     // "Assets/Data/Multiple_choice_grammar.txt"
-    fileName = "Assets/logs/" + strToday + ".txt";
-    if (File.Exists(fileName))
+    testFileName = "Assets/logs/" + strToday + ".txt";
+    Directory.CreateDirectory(Application.streamingAssetsPath + "/logs/");
+    buildFileName = Application.streamingAssetsPath + "/logs/" + strToday + ".txt";
+    if (File.Exists(testFileName))
     {
-        File.Delete(fileName);
+        File.Delete(testFileName);
+    }
+    if (File.Exists(buildFileName))
+    {
+        File.Delete(buildFileName);
     }
     // Create Empty File
-    using (FileStream fs = File.Create(fileName))
+    using (FileStream fs = File.Create(testFileName))
+    {
+    }
+    // Create Empty File
+    using (FileStream fs = File.Create(buildFileName))
     {
     }
     this.writeDebug("Initialization of Logging File Completed");
@@ -33,7 +44,11 @@ public class Logger
     var todayDate = DateTime.Now;
     Debug.Log(todayDate.ToString("[HH:mm:ss.ffff]") + "[DEBUG]\t" + txt);
     // Add some text to file
-    using (StreamWriter fs = File.AppendText(fileName))
+    using (StreamWriter fs = File.AppendText(testFileName))
+    {
+        fs.WriteLine(todayDate.ToString("[HH:mm:ss.ffff]") + "[DEBUG]\t" + txt);
+    }
+    using (StreamWriter fs = File.AppendText(buildFileName))
     {
         fs.WriteLine(todayDate.ToString("[HH:mm:ss.ffff]") + "[DEBUG]\t" + txt);
     }
@@ -42,7 +57,11 @@ public class Logger
     var todayDate = DateTime.Now;
     Debug.Log(todayDate.ToString("[HH:mm:ss.ffff]") + "[ERROR]\t" + txt);
     // Add some text to file
-    using (StreamWriter fs = File.AppendText(fileName))
+    using (StreamWriter fs = File.AppendText(testFileName))
+    {
+        fs.WriteLine(todayDate.ToString("[HH:mm:ss.ffff]") + "[ERROR]\t" + txt);
+    }
+    using (StreamWriter fs = File.AppendText(buildFileName))
     {
         fs.WriteLine(todayDate.ToString("[HH:mm:ss.ffff]") + "[ERROR]\t" + txt);
     }
